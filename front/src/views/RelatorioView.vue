48b082dde036ca532d009ccec52507e556cdd214
Lucas <lukas.figueiredo@hotmail.com>
2024-09-15 02:19:15 -0300

    Extração de relatório com notificação

--- front/src/views/RelatorioView.vue
+++ front/src/views/RelatorioView.vue
@@ -6,6 +6,9 @@ const fileName = ref('');
 const downloadLink = ref();
 const status = ref('')
 const polling = ref();
+const snackbar = ref(false); 
+const snackbarMessage = ref(''); 
+let intervalId = null; 
 
 const generateReport = async () => {
     const { data } = await Report.unvaccinatedEmployees();
@@ -15,24 +18,42 @@ const generateReport = async () => {
     }
 }
 
-const startPolling = () => {
-    polling.value = setInterval(checkReportStatus, 25000);
-}
-
 const checkReportStatus = async () => {
     const data = await Report.checkStatus();
     status.value = data.status;   
     if (data.status === 'completed') {
         fileName.value = data.fileName
         downloadLink.value = data.download_link
-        clearInterval(polling.value);
+
+        snackbarMessage.value = 'Your report is ready!';
+        snackbar.value = true;
+        stopPolling();       
     }
 }
 
+// Download the report
+const downloadReport = () => {
+  if (downloadLink.value) {
+    window.location.href = downloadLink.value;
+  }
+};
+
+const startPolling = () => {
+  if (!polling.value) {
+    polling.value = true;
+    intervalId = setInterval(checkReportStatus, 5000);
+  }
+};
+
+const stopPolling = () => {
+  if (intervalId) {
+    clearInterval(intervalId);
+    polling.value = false;
+  }
+};
+
 onBeforeUnmount(async () => {
-    if (polling.value) {
-        clearInterval(polling.value);
-    }
+    stopPolling();
 })
 </script>
 <template>
@@ -53,9 +74,28 @@ onBeforeUnmount(async () => {
     </v-row>
     <v-row>
         <v-col>
-            <span v-if="status != 'completed' && status != ''">Relatório sendo gerado...</span>
+            <span v-if="status != 'completed' && status != ''">Relatório sendo gerado, quando estiver pronto você receberá uma notificação</span>
             <span v-if="status == 'completed'"><a :href="downloadLink" download>Baixar Relatório</a></span>
         </v-col>
     </v-row>
+
+    <v-snackbar
+      v-model="snackbar"
+      :timeout="20000"
+      vertical
+    >
+      <div class="text-subtitle-1 pb-2">Relatório está pronto</div>
+      <p>Clique no botão abaixo para baixar</p>    
+      <template v-slot:actions>
+        <v-btn
+          color="#4A4A4A"
+          style="background-color: #FFF"
+          variant="text"
+          @click="downloadReport"
+        >
+            Baixar Relatório
+        </v-btn>
+    </template>      
+    </v-snackbar>
 
 </template>
